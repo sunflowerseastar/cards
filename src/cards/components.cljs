3a902889c42b5f7f5ff52163de96958f9ebe7569
Grant Surlyn <grant@betabrand.com>
2019-09-14 14:52:42 -0700

    deal and re-deal shuffled blackjack hands

--- src/cards/components.cljs
+++ src/cards/components.cljs
@@ -4,26 +4,33 @@
    [cards.deck :as deck]
    [reagent.core :as reagent :refer [atom]]))
 
-(def hands (atom {:dealer {:down-card {:suit 'h :rank 2} :up-card {:suit 'h :rank 9}}
-                  :you {:down-card {:suit 'd :rank 10} :up-card {:suit 's :rank 12}}}))
-
-(defn card [player up-or-down]
-  (let [{{{suit :suit, rank :rank} up-or-down} player} @hands]
+(defn card [hands player up-or-down]
+  (let [{{{suit :suit, rank :rank} up-or-down} player} hands]
     [:span
      (svgs/svg-of suit)
      (deck/translate-rank-of rank)]))
 
+(defn generate-hands [local-deck]
+  (let [y-d-1-card (first local-deck)
+        y-d-2-card (second local-deck)
+        d-d-card (local-deck 2)
+        d-u-card (local-deck 3)]
+    {:you {:down-1-card y-d-1-card, :down-2-card y-d-2-card}
+     :dealer {:down-card d-d-card, :up-card d-u-card}}))
+
 (defn blackjack []
-  [:div
-   [:div.dealer
-    [:p "dealer"]
-    [card :dealer :up-card]
-    [card :dealer :down-card]]
-   [:div.you
-    [:p "you"]
-    [card :you :up-card]
-    [card :you :down-card]
-    ]])
+  (let [hands (atom (generate-hands (deck/shuffle-deck (deck/generate-deck))))]
+    [:div
+     [:button {:on-click #(swap! hands (generate-hands (deck/shuffle-deck (deck/generate-deck))))} "re-deal"]
+     [:div.dealer
+      [:p "dealer"]
+      [card @hands :dealer :down-card]
+      [card @hands :dealer :up-card]]
+     [:div.you
+      [:p "you"]
+      [card @hands :you :down-1-card]
+      [card @hands :you :down-2-card]
+      ]]))
 
 (defn card-list []
   (let [local-deck (atom (deck/generate-deck))]
@@ -33,7 +40,7 @@
        (svgs/svg-of 'c)
        (svgs/svg-of 'd)
        (svgs/svg-of 'h)
-       [:button {:on-click #(swap! local-deck deck/shuffler deck/weighted-shuffle)} "shuffle"]
+       [:button {:on-click #(swap! local-deck deck/shuffle-deck)} "shuffle"]
        [:button {:on-click #(swap! local-deck deck/generate-deck)} "sort"]
        [:ul
         (for [current-card @local-deck] [:p {:key (apply str [(:suit current-card) (:rank current-card)])}
--- src/cards/deck.cljs
+++ src/cards/deck.cljs
@@ -1,7 +1,7 @@
 (ns cards.deck)
 
 (defn generate-deck []
-  (for [suit ['s 'c 'd 'h] rank [2 3 4 5 6 7 8 9 10 11 12 13 14]] {:suit suit :rank rank}))
+  (vec (for [suit ['s 'c 'd 'h] rank [2 3 4 5 6 7 8 9 10 11 12 13 14]] {:suit suit :rank rank})))
 
 (def deck (atom (generate-deck)))
 
@@ -28,3 +28,6 @@
 
 (defn shuffler [deck shuffle-fn]
   (let [l-r-deck (divide-deck deck)] (shuffle-fn (first l-r-deck) (second l-r-deck))))
+
+(defn shuffle-deck [deck]
+  (vec (shuffler deck weighted-shuffle)))

73880975e788dcc5d15f08f0897466a497f71466
Grant Surlyn <grant@betabrand.com>
2019-09-16 16:06:58 -0700

    accommodate high/low (hard/soft) aces

--- src/cards/blackjack_helpers.cljs
+++ src/cards/blackjack_helpers.cljs
@@ -4,16 +4,33 @@
   (let [{:keys [card-1 card-2 hits]} hand]
     (vec (filter not-empty (flatten (conj [card-1] [card-2] hits))))))
 
-(defn translate-rank-to-value [rank]
+(defn translate-rank-hard [rank]
   (cond (< rank 11) rank
         (< rank 14) 10
         (= rank 14) 11))
 
-(defn value-old [hand]
-  (reduce + (map #(translate-rank-to-value (% :rank)) (cards-from-hand hand))))
+(defn translate-rank-soft [rank]
+  (cond (< rank 11) rank
+        (< rank 14) 10
+        (= rank 14) 1))
+
+(defn has-ace-p [hand]
+  (some #(= (% :rank) 14) (cards-from-hand hand)))
+
+(defn choose-value [v1 v2]
+  (let [lower-value (if (< v2 v1) v2 v1)
+        higher-value (if (< v2 v1) v1 v2)]
+    (cond (and (> v1 21) (< v2 21)) v2
+          (and (> v2 21) (< v1 21)) v1
+          (and (> v1 21) (> v2 21)) lower-value
+          (and (< v1 21) (< v2 21)) higher-value
+          :else v1)))
+
+(defn value-sum [hand translater]
+  (reduce + (map #(translater (% :rank)) (cards-from-hand hand))))
 
 (defn value [hand]
-  (value-old hand))
-
-(defn values [hand]
-  (value hand))
+  (if (has-ace-p hand)
+    (choose-value (value-sum hand translate-rank-hard)
+                  (value-sum hand translate-rank-soft))
+    (value-sum hand translate-rank-soft)))

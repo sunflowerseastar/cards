6147cd2bad4ba3a77b148894af6b87d67db5ea10
Grant Surlyn <grant@sunflowerseastar.com>
2022-05-05 22:00:46 -0400

    refactor (:dealer @hands) so that it's just one hand
    Previously, it was a "vector of hands," even though the dealer would always only
    have one hand (since the dealer doesn't split).

--- src/cards/blackjack.cljs
+++ src/cards/blackjack.cljs
@@ -5,6 +5,10 @@
    [cards.deck :refer [generate-shuffled-deck]]
    [reagent.core :as reagent :refer [atom]]))
 
+
+;; dealer hits everything below, stands on everything equal and above
+
+
 (def dealer-hit-cutoff 17)
 
 
@@ -30,24 +34,26 @@
 (def deck (atom (generate-shuffled-deck)))
 
 ;; hands ex. {:you [[{:suit diamond :rank 7} {:suit diamond, :rank 14}]],
-;;            :dealer [[{:suit heart, :rank 2} {:suit heart, :rank 8}]]}
-
-;; Dealer gets a vector of hands, although in practice the dealer always has only one hand.
-;; TODO consider changing dealer hand data structure
+;;            :dealer [{:suit heart, :rank 2} {:suit heart, :rank 8}]}
+;;
+;; Notice that :you get a vector of hands (since you can split), whereas the
+;; dealer always only has one hand.
+;;
+;; For most of the game, :you will have a vector of hands containing just that
+;; that one hand. But when you split, there will be additional hands, and
+;; :current-split will inc past 0, through each of the split hands.
 
 (def hands (atom {}))
 ;; TODO change how cards are dealt (draw-counter)
 (def draw-counter (atom 4))
 
-;; TODO review deal and verify that this is vegas standard
-;; TODO check if dealer has blackjack after dealing - if so, end game
 (defn deal-hands [local-deck]
   (let [your-card-1 (first local-deck)
         dealer-card-1 (second local-deck)
         your-card-2 (local-deck 2)
         dealer-card-2 (local-deck 3)]
     {:you [[your-card-1 your-card-2]]
-     :dealer [[dealer-card-1 dealer-card-2]]}))
+     :dealer [dealer-card-1 dealer-card-2]}))
 
 (defn reset-game! []
   (do (reset! deck generate-shuffled-deck)
@@ -67,7 +73,7 @@
 (defn conclude-game! []
   (do
     ;; update scoring
-    (doseq [outcome (map #(hands->win-lose-push % (first (:dealer @hands))) (:you @hands))]
+    (doseq [outcome (map #(hands->win-lose-push % (:dealer @hands)) (:you @hands))]
       (case outcome
         :win (swap! game update :wins inc)
         :lose (swap! game update :losses inc)
@@ -75,20 +81,27 @@
     ;; end gameplay
     (swap! game assoc :state :stopped :turn :none)))
 
-(defn add-hit-card-to-hand! [player card]
-  (swap! hands update-in [player (:current-split @game)] conj card))
+(defn add-hit-card-to-hand!
+  "Draw a card and add it to the hand currently being played."
+  []
+  (let [player (:turn @game)
+        hit-card (draw-hit-card!)]
+    (if (= player :you)
+      (swap! hands update-in [:you (:current-split @game)] conj hit-card)
+      (swap! hands update :dealer conj hit-card))))
 
 (defn dealer-plays! []
-  (do (swap! game assoc :turn :dealer :current-split 0)
+  (do (swap! game assoc :turn :dealer)
       (let [you-busted-all-hands
             (->> (:you @hands) (map hand->value) (filter #(<= % 21)) empty?)]
         (if you-busted-all-hands
           (conclude-game!)
           (while (= (:state @game) :running)
-            (let [dealer-hand-value (hand->value (nth (:dealer @hands) 0))]
-              (if (< dealer-hand-value dealer-hit-cutoff)
-                (add-hit-card-to-hand! :dealer (draw-hit-card!))
-                (conclude-game!))))))))
+            (if (< (hand->value (:dealer @hands)) dealer-hit-cutoff)
+              (add-hit-card-to-hand!)
+              (conclude-game!)))))))
+
+;; Interactive commands
 
 (defn deal! []
   (do
@@ -101,7 +114,7 @@
     (reset! hands (deal-hands @deck))
     (reset! draw-counter 4)
     (swap! game assoc :state :running :turn :you :current-split 0)
-    (if (= (->> (:dealer @hands) first hand->value) 21)
+    (if (= (hand->value (:dealer @hands)) 21)
       ;; if the dealer has blackjack, then there is no interactive gameplay for the round
       (conclude-game!)
       ;; otherwise, if you have blackjack, go ahead and turn gameplay over to the dealer
@@ -118,14 +131,18 @@
       (swap! hands update-in [:you (:current-split @game)] #(-> (take 1 %) vec))))
 
 (defn hit! []
-  (do (add-hit-card-to-hand! :you (draw-hit-card!))
+  (do (add-hit-card-to-hand!)
       (let [your-value (hand->value (nth (:you @hands) (:current-split @game)))
-            more-splits-remaining-p (and (= (:turn @game) :you) (> (- (count (:you @hands)) 1) (:current-split @game)))]
-        (cond (and more-splits-remaining-p (>= your-value 21)) (play-next-split!)
+            are-more-splits-remaining (and (= (:turn @game) :you) (> (- (count (:you @hands)) 1) (:current-split @game)))]
+        (cond (and are-more-splits-remaining (>= your-value 21)) (play-next-split!)
               (>= your-value 21) (dealer-plays!)))))
 
 (defn toggle-modal! []
   (swap! game assoc :is-modal-showing (not (:is-modal-showing @game))))
+
+
+;; Top-level gameplay markup
+
 
 (defn blackjack []
   [:div.blackjack-container.padding-lr-sm
@@ -142,7 +159,7 @@
      (when (not (empty? @hands))
        [:<>
 
-        (let [hand (first (:dealer @hands))
+        (let [hand (:dealer @hands)
               is-active false
               is-a-card-in-the-hole (= (:turn @game) :you)]
           (hand-component hand
@@ -159,7 +176,7 @@
                       (let [is-active (and (= (:turn @game) :you)
                                            (= (:current-split @game) i))
                             hand-outcome (and (= (:state @game) :stopped)
-                                              (hands->win-lose-push hand (first (:dealer @hands))))
+                                              (hands->win-lose-push hand (:dealer @hands)))
                             is-win (and (= (:state @game) :stopped)
                                         (= hand-outcome :win))]
                         (hand-component hand

00b2c3dee6b2757411c93c18ea6fefe235f475c2
Grant Surlyn <grant@sunflowerseastar.com>
2022-05-10 11:55:01 -0400

    add simple routing functionality, and UI inside modal

--- src/cards/core.cljs
+++ src/cards/core.cljs
@@ -2,36 +2,48 @@
   (:require
    [cards.components :as c]
    [cards.blackjack :refer [blackjack-main]]
+   [alandipert.storage-atom :refer [local-storage]]
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom create-class]]
    [reagent.dom :as rdom]))
 
-;; (def screen (atom blackjack-main))
-(def screen (atom c/card-display))
-
 (defonce has-initially-loaded (atom false))
+
+(def current-route (local-storage (atom :blackjack) :current-route))
+
+(def routes
+  {:card-display c/card-display
+   :blackjack blackjack-main})
+
+(defn route-to-blackjack!
+  "Send user to the blackjack page."
+  [] (reset! current-route :blackjack))
+
+(defn route-to-card-display!
+  "Send user to the card-display page."
+  [] (reset! current-route :card-display))
+
+(defn route-to!
+  "Send user to different page."
+  [new-route] (reset! current-route new-route))
 
 (defn app []
   (letfn [(keyboard-listeners [e]
             (let [key (.-key e)
                   is-b (= (.-keyCode e) 66)
                   is-d (= (.-keyCode e) 68)]
-              (cond is-d (reset! screen c/card-display)
-                    is-b (reset! screen blackjack-main))))]
+              (cond is-d (do (println "is-d") (route-to! :card-display))
+                    is-b (do (println "is-b") (route-to! :blackjack)))))]
     (create-class
      {:component-did-mount (fn [] (do (js/setTimeout #(reset! has-initially-loaded true) 0)
                                       (.addEventListener js/document "keydown" keyboard-listeners)))
       :reagent-render
       (fn [this]
-        [:div.app.fade-in-1 {:class [(if @has-initially-loaded "has-initially-loaded")]}
-        ;; [:div.button-group.white-bg
-        ;;  [:button {:on-click #(reset! screen c/card-display)} "card-display"]
-        ;;  [:button {:on-click #(reset! screen blackjack-main)} "blackjack"]]
-         (c/modal)
-
-         (c/header)
-
-         [@screen]])})))
+        (let [current-route-component (@current-route routes)]
+          [:div.app.fade-in-1 {:class [(if @has-initially-loaded "has-initially-loaded")]}
+           (c/modal current-route routes route-to!)
+           (c/header)
+           (current-route-component)]))})))
 
 (defn mount [el]
   (rdom/render [app] el))

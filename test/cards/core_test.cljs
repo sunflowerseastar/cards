33371a0b2be9a711f7d6f738a9a8c13f01719820
Grant Surlyn <grant@sunflowerseastar.com>
2022-05-12 11:09:17 -0400

    tests

--- test/cards/core_test.cljs
+++ test/cards/core_test.cljs
@@ -4,8 +4,32 @@
    [cards.blackjack-helpers :refer [hands->win-lose-push]]
    [cljs.test :refer-macros [are deftest is testing]]))
 
+(def top-card {:suit 'heart :rank 14})
+(def bottom-card {:suit 'spade :rank 14})
+
 (deftest sorted-deck-test
-  (is (= true true)))
+  (are [x y] (= x y)
+    (count (deck/sorted-deck)) 52
+    (first (deck/sorted-deck)) top-card
+    (last (deck/sorted-deck)) bottom-card))
+
+(deftest generate-shoe-test
+  (are [x y] (= x y)
+    (count (deck/generate-shoe 1)) 52
+    (count (deck/generate-shoe 2)) 104
+    (count (deck/generate-shoe 4)) 208
+    (first (deck/generate-shoe 4)) top-card
+    (-> (deck/generate-shoe 4) (nth 51)) bottom-card
+    (-> (deck/generate-shoe 4) (nth 52)) top-card
+    (last (deck/generate-shoe 4)) bottom-card))
+
+(deftest riffle-shuffle-test
+  ;; two perfect shuffles will match
+  (is (= (deck/riffle-shuffle (deck/sorted-deck) 1) (deck/riffle-shuffle (deck/sorted-deck) 1)))
+  ;; two imperfect shuffles will not match
+  (is (not= (deck/riffle-shuffle (deck/sorted-deck) 0.9) (deck/riffle-shuffle (deck/sorted-deck) 0.9)))
+  (are [x y] (= x y)
+    (count (deck/riffle-shuffle (deck/sorted-deck))) 52))
 
 ;; "h-" stands for "hand-", or "sample-test-hand-"
 (def h-blackjack [{:suit 'spade, :rank 14} {:suit 'spade, :rank 13}])

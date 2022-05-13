529b07a92982f8465eaa133f586b3c6c916c89dc
Grant Surlyn <grant@sunflowerseastar.com>
2022-05-13 12:19:35 -0400

    update card-display

--- src/cards/card_display.cljs
+++ src/cards/card_display.cljs
@@ -17,24 +17,25 @@
   "Show the cards, along with buttons to shuffle and sort."
   []
   [:div.card-display.padding-lr-sm.max-width-900
+
    [:div.card-display-controls
     [:div.button-group
      [:button {:class (if (> @options/num-decks-in-shoe 2) "inactive") :on-click #(swap! local-shoe deck/riffle)} "riffle"]
      [:button {:class (if (> @options/num-decks-in-shoe 2) "inactive") :on-click #(swap! local-shoe deck/strip)} "strip"]
+     [:button {:class (if (< @options/num-decks-in-shoe 3) "inactive") :on-click #(swap! local-shoe deck/strip)} "shuffle shoe"]]
+    [:div.button-group
      [:button {:on-click #(swap! local-shoe deck/cut)} "cut"]
-     [:button {:on-click regenerate!} "regenerate"]]
+     [:button {:on-click #(swap! local-shoe deck/cut-one-third-top)} "cut top 1/3"]
+     [:button {:on-click #(swap! local-shoe deck/cut-one-third-bottom)} "box"]
+     [:button {:on-click regenerate!} "reset"]]
+
     (options/shuffle-precision-slider)
     (options/num-decks-in-shoe-slider regenerate!)
-    [:p "Cards shown: " (count @local-shoe)]]
 
+    [:p "Cards shown: " (count @local-shoe)]]
 
        ;; (card-component (first local-shoe))
        ;; (card-component {:suit :spade :rank 12})
        ;; (into [:div.card-display] (map card-down-component @local-shoe)) ;; all cards are face down
        ;; (into [:div.card-display] (map card-down-component (take 1 @local-shoe))) ;; 1 card, face down
-
-
    (into [:div.card-display-inner] (map c/card-component @local-shoe))])
-
-;; (defn card-display-view []
-;;   [card-display-inner])
